051c0685e940ed961584df1c9a4fde67ea012de3
Nisha <nisha@Nishas-MacBook-Pro.local>
2020-10-13 01:23:17 -0400

    updates

---
+++ build/precache-manifest.909527a6f5aea16e1184a4aa52d689b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e95b649a87a1624bfd163812b046fea",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1cd241f679761064ff7d",
+    "url": "/tic-tac-toe/static/css/main.bb2189c8.chunk.css"
+  },
+  {
+    "revision": "3dc33abc4ea725905a1b",
+    "url": "/tic-tac-toe/static/js/2.1dac75d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.1dac75d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd241f679761064ff7d",
+    "url": "/tic-tac-toe/static/js/main.f4cc2117.chunk.js"
+  },
+  {
+    "revision": "4d40351a766e1df1af7f",
+    "url": "/tic-tac-toe/static/js/runtime-main.47255ecd.js"
+  }
+]);
